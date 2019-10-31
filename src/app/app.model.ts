d25adde20311c1b380a2d4b3358fd5fb541a5c6f
John Chappelear <john.chappelear@gmail.com>
2019-10-30 20:15:22 -0400

    Still trying to fix missing projectId

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -1,6 +1,4 @@
-
 export interface Task {
     id: string;
-  
     description: string;
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { TaskService } from './app.service';
+import { environment } from "./../environments/environment";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+// Third Party Modules
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule, AngularFirestore } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [TaskService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
